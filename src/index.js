53a4f5b08d812f0ce05fb66757abd25fb78618f2
rohmanhakim <rohmanhakim@live.com>
2020-01-26 12:52:45 +0700

    Add shimmering animation when loading books

--- src/index.js
+++ src/index.js
@@ -1,50 +1,91 @@
-function component() {
+import './styles/app.css';
+
+function initComponent() {
   const apiKey = process.env.GOODREADS_API_KEY;
   const userId = process.env.GOODREADS_USER_ID;
   const goodreadsUrl = "https://www.goodreads.com/";
   const shelf = "read";
-  const goodreadsEndpoint = `review/list/${userId}.xml?key=${apiKey}&v=2&shelf=${shelf}`;
+  const perPage = 20;
+  const goodreadsEndpoint = `review/list/${userId}.xml?key=${apiKey}&v=2&shelf=${shelf}&per_page=${perPage}`;
   const corsAnywhereUrl = "https://cors-anywhere.herokuapp.com/"
   const url = corsAnywhereUrl + goodreadsUrl + goodreadsEndpoint;
 
-  const element = document.createElement('div');
-
   var nextPage = 1;
-  var bookElement  = ""
+
+  var showLoading = function () {
+    const loadingWrapper = document.createElement('ul');
+    loadingWrapper.setAttribute("id", "loading-wrapper")
+    loadingWrapper.setAttribute("class", "loading-wrapper")
+    var i;
+    for (i = 0; i < perPage; i++) {
+      var x = Math.floor((Math.random() * 3) + 1);
+      const shimmering = document.createElement('div');
+      switch (x) {
+        case 1:
+          shimmering.setAttribute("class", "comment br animate w240");
+          break;
+        case 2:
+          shimmering.setAttribute("class", "comment br animate w320");
+          break;
+        case 3:
+          shimmering.setAttribute("class", "comment br animate w480");
+          break;
+      }
+      const shimmeringElement = document.createElement('li');
+      shimmeringElement.appendChild(shimmering);
+      loadingWrapper.appendChild(shimmeringElement);
+    }
+
+    document.body.appendChild(loadingWrapper);
+
+    const bookshelf = document.createElement('ul');
+    bookshelf.setAttribute("id", "bookshelf");
+
+    document.body.insertBefore(bookshelf, document.body.firstChild);
+  }
+
+  var hideLoading =  function () {
+    document.body.removeChild(document.getElementById('loading-wrapper'));
+  }
 
   var fetchBooks = function () {
     fetch(url + "&page=" + nextPage)
-    .then((response) => {
-      return response.text();
-    })
-    .then((xmlResponse) => {
-      var domParser = new DOMParser();
-      var xmlDocument = domParser.parseFromString(xmlResponse, "text/xml");
+      .then((response) => {
+        return response.text();
+      })
+      .then((xmlResponse) => {
+        var domParser = new DOMParser();
+        var xmlDocument = domParser.parseFromString(xmlResponse, "text/xml");
 
-      var total = xmlDocument.getElementsByTagName('reviews')[0].getAttribute('total');
-      var end = xmlDocument.getElementsByTagName('reviews')[0].getAttribute('end');
+        var total = xmlDocument.getElementsByTagName('reviews')[0].getAttribute('total');
+        var end = xmlDocument.getElementsByTagName('reviews')[0].getAttribute('end');
 
-      var reviews = xmlDocument.getElementsByTagName('review')
-      var mapped = Array.from(reviews).map(review => {
-        return "<li><a href=\"" +
-          review.getElementsByTagName('book')[0].getElementsByTagName('link')[0].innerHTML + "\">" +
-          review.getElementsByTagName('book')[0].getElementsByTagName('title')[0].innerHTML +
-          "</a></li>";
-      }).join("");
-      bookElement += mapped;
-      console.log(mapped);
+        var reviews = xmlDocument.getElementsByTagName('review')
+        var mapped = Array.from(reviews).map(review => {
+          return "<a href=\"" +
+            review.getElementsByTagName('book')[0].getElementsByTagName('link')[0].innerHTML + "\">" +
+            review.getElementsByTagName('book')[0].getElementsByTagName('title')[0].innerHTML +
+            "</a>";
+        });
 
-      if (end < total) {
-        nextPage  = nextPage + 1;
-        element.innerHTML = "<ul class=\"book-snippet\" id=\"book-snippet\">" + bookElement + "</ul>";
-        document.body.appendChild(element);
-        fetchBooks();
-      }
-    });
+        mapped.forEach(item => {
+          const book = document.createElement('li');
+          book.innerHTML = item;
+          document.getElementById('bookshelf').appendChild(book);
+        })
+        nextPage = nextPage + 1;
+
+        if (end < total) {
+          fetchBooks();
+        } else {
+          hideLoading();
+        }
+      });
   }
 
+  showLoading();
   fetchBooks();
 }
 
-component();
+initComponent();
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const webpack = require('webpack');
+
 module.exports = {
     entry: './src/index.js',
     mode: 'development',
     plugins: [
         new webpack.EnvironmentPlugin(['GOODREADS_API_KEY', 'GOODREADS_USER_ID'])
-    ]
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+       ]
+    }
 };
 
