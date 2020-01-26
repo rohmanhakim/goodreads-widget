a6b30acba6ba64e65169c461a84c5a1bac5b5ebe
rohmanhakim <rohmanhakim@live.com>
2020-01-26 09:45:37 +0700

    Add capability to show all pages of books in a bookshelf

--- src/index.js
+++ src/index.js
@@ -4,29 +4,46 @@ function component() {
   const goodreadsUrl = "https://www.goodreads.com/";
   const shelf = "read";
   const goodreadsEndpoint = `review/list/${userId}.xml?key=${apiKey}&v=2&shelf=${shelf}`;
-  const corsAnywhereUrl = "https://cors-anywhere.herokuapp.com/" 
+  const corsAnywhereUrl = "https://cors-anywhere.herokuapp.com/"
   const url = corsAnywhereUrl + goodreadsUrl + goodreadsEndpoint;
 
   const element = document.createElement('div');
-  
-  fetch(url + "&page=" + "1")
-  .then((response) => {
-    return response.text();
-  })
-  .then((xmlResponse) => {
-    var domParser = new DOMParser();
-    var xmlDocument = domParser.parseFromString(xmlResponse, "text/xml");
-    var reviews = xmlDocument.getElementsByTagName('review')
-    var titles = Array.from(reviews).map( review => { 
-      return "<li><a href=\"" +  
-        review.getElementsByTagName('book')[0].getElementsByTagName('link')[0].innerHTML + "\">" +
-        review.getElementsByTagName('book')[0].getElementsByTagName('title')[0].innerHTML +
-      "</a></li>";
-    }).join("");
-    console.log(titles);
-    element.innerHTML  = "<ul class=\"book-snippet\" id=\"book-snippet\">" + titles + "</ul>";
-    document.body.appendChild(element);
-  });
+
+  var nextPage = 1;
+  var bookElement  = ""
+
+  var fetchBooks = function () {
+    fetch(url + "&page=" + nextPage)
+    .then((response) => {
+      return response.text();
+    })
+    .then((xmlResponse) => {
+      var domParser = new DOMParser();
+      var xmlDocument = domParser.parseFromString(xmlResponse, "text/xml");
+
+      var total = xmlDocument.getElementsByTagName('reviews')[0].getAttribute('total');
+      var end = xmlDocument.getElementsByTagName('reviews')[0].getAttribute('end');
+
+      var reviews = xmlDocument.getElementsByTagName('review')
+      var mapped = Array.from(reviews).map(review => {
+        return "<li><a href=\"" +
+          review.getElementsByTagName('book')[0].getElementsByTagName('link')[0].innerHTML + "\">" +
+          review.getElementsByTagName('book')[0].getElementsByTagName('title')[0].innerHTML +
+          "</a></li>";
+      }).join("");
+      bookElement += mapped;
+      console.log(mapped);
+
+      if (end < total) {
+        nextPage  = nextPage + 1;
+        element.innerHTML = "<ul class=\"book-snippet\" id=\"book-snippet\">" + bookElement + "</ul>";
+        document.body.appendChild(element);
+        fetchBooks();
+      }
+    });
+  }
+
+  fetchBooks();
 }
 
 component();
